af16d4f445e8abdc81531d3ec9c6ae7d417c0ac1
َAli IKHLEF <39748592+151ali@users.noreply.github.com>
2018-10-09 15:16:59 +0100

    Create script.py

---
+++ LEDstatus/script.py
@@ -0,0 +1,13 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0)
+  
+while True:
+	data = (ser.readline()).decode('utf8')
+
+	if(data == '0' ): print('the LED is Off')
+	time.sleep(1)
+	if(data == '0' ): print('the LED is ON')
+  
+  # written by AI : 09/10/2018 15:16:18
